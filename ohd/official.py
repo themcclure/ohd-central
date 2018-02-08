10f99c7492c1dc26df84ddbade71c3dfab6c211b
themcclure <mike@themcclure.com>
2018-02-07 20:55:36 -0800

    Fixed the game data being loaded as {} rather than []. Continued to transfer Game class over to new codebase.

--- ohd/official.py
+++ ohd/official.py
@@ -48,14 +48,21 @@ class Official:
         :return: None
         """
         # Get all the rows apart from the top line (header)
-        games = history_tab.get_all_records()[1:]
+        games = history_tab.get_all_values()[1:]
         # Sort list by date (descending)
-        # TODO: sort list
+        games.sort(reverse=True)
         # Iterate through games, one at a time until there's no more
         for game in games:
             # TODO: validate the input data and form a Game, then add it to the Official
-            print u'Date for the game is {}.'.format(datetime.date(game[0]))
-            self.add_game(game)
+            try:
+                # Validation: Is it a date?
+                gdate = datetime.datetime.strptime(game[0], '%Y-%m-%d').date()
+
+                # print u'Date for the game is {}.'.format(datetime.datetime.strptime(game[0], '%Y-%m-%d').date())
+                self.add_game(game)
+            except Exception as e:
+                # This game isn't valid, go to the next one
+                print u'Game not valid because: {}'.format(e)
 
     def add_game(self, game):
         """
@@ -81,23 +88,24 @@ class Game:
         Age is the the number of whole years since the reference date (freezeDate)
         Primacy is 1 for games worked in the primary position, 2 for secondary positions
     """
-    def __init__(self, assn, gtype, role, age, primacy, date):
+    def __init__(self, gdate, assn, gtype, role, primacy):
         # default "error" values
         self.assn = None
         self.type = None
         self.role = None
-        self.age = None
-        self.date = None
+        self.gdate = None
         self.primacy = None
 
         # if all the inputs are valid, then populate the data
-        if (assn in assns) and (gtype in types) and (role in roles) and (age >= 0) and (primacy >= 1):
+        if (assn in assns) and (gtype in types) and (role in roles) and (isinstance(gdate, datetime.date)) and (primacy in (1,2)):
             self.assn = assn
             self.type = gtype
             self.role = role
-            self.age = age
-            self.date = date
+            # self.age = age
+            self.gdate = gdate
             self.primacy = primacy
+        else:
+            raise Exception('Validation of inputs failed... What? Too lazy to check the inputs for validity??')
 
     def __repr__(self):
         return "<Assn %s, Role %s>" % (self.assn, self.role)
